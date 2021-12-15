1c9c6226e3cd0c9266cc843b646849fd10435a54
ryoryoryoooo <P1211065@gmail.com>
2021-12-16 08:09:52 +0900

    選択したボタン以外は、非選択にする実装

--- ButtonSample1215/ViewController.swift
+++ ButtonSample1215/ViewController.swift
@@ -14,31 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var Button3: UIButton!
     @IBOutlet weak var Button4: UIButton!
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        Button1.backgroundColor = .brown
         // Do any additional setup after loading the view.
     }
 
 
     @IBAction func button1(_ sender: Any) {
-        if Button1.backgroundColor == .brown {
-            Button1.backgroundColor = .blue
-          }
-        else if Button1.backgroundColor == .blue {
-            Button1.backgroundColor = .brown
-          }
-
+        changebuttonsIsSelected(UIButton: Button1)
     }
 
     @IBAction func button2(_ sender: Any) {
+        changebuttonsIsSelected(UIButton: Button2)
     }
 
     @IBAction func button3(_ sender: Any) {
+        changebuttonsIsSelected(UIButton: Button3)
     }
 
     @IBAction func button4(_ sender: Any) {
+        changebuttonsIsSelected(UIButton: Button4)
+    }
+
+    func changebuttonsIsSelected(UIButton:UIButton){
+        let buttons = [Button1,Button2,Button3,Button4]
+        buttons.map(){
+            $0?.isSelected = false
+        }
+        UIButton.isSelected = true
     }
 }
 
