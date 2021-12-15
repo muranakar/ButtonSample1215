dfba5075d6ad35b99760ca3a3b4a6ac09fc33ce5
ryoryoryoooo <P1211065@gmail.com>
2021-12-15 22:29:27 +0900

    ファーストコミット

--- ButtonSample1215/ViewController.swift
+++ ButtonSample1215/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        Button1.backgroundColor = .brown
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func button1(_ sender: Any) {
+        if Button1.backgroundColor == .brown {
+            Button1.backgroundColor = .blue
+          }
+        else if Button1.backgroundColor == .blue {
+            Button1.backgroundColor = .brown
+          }
+
+    }
+
+    @IBAction func button2(_ sender: Any) {
+    }
+
+    @IBAction func button3(_ sender: Any) {
+    }
+
+    @IBAction func button4(_ sender: Any) {
+    }
 }
 
